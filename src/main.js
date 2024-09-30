c317bce1fe10388afe46be2299d54672349cfc20
Dani <dhuber88@sunrise.ch>
2024-09-30 16:42:07 +0200

    axios in main.js hinzugefügt

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+
+axios.defaults.withCredentials = true;  // Aktiviert das Senden von Cookies bei Anfragen
+axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
 const app = createApp(App)
 
