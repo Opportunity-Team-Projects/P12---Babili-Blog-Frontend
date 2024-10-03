718bc1f080038c65167e27deba23a06fd13b3df5
philipp-burri <burri.philipp13@gmail.com>
2024-10-03 12:08:50 +0200

    functioning post fetch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
-<script setup>
-/* import TheWelcome from '../components/TheWelcome.vue' */
-</script>
-
 <template>
-  <main>
-    
-  </main>
+  <div>
+    <div v-for="post in posts" :key="post.id">
+      <h2>{{ post.contentTitle }}</h2>
+      <p>{{ post.contentPreview }}</p>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { authClient } from '../services/AuthService';
+
+const posts = ref([]);
+
+const fetchAllPosts = async () => {
+  try {
+    const res = await authClient.get('/api/index');
+    posts.value = res.data;
+    console.log('Fetched all posts:', res.data);
+  } catch (error) {
+    console.error('Error fetching all posts:', error);
+  }
+};
+
+onMounted(fetchAllPosts);
+</script>
