1eb926066326ee481ba8b54e48637c9ab88f4608
harryszh <harrys.zh12@gmail.com>
2024-10-03 14:56:23 +0200

    regi

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,12 @@ const router = createRouter({
       component: () => import('../views/LoginView.vue')
     },
     {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue')
+
+    },
+    {
       path: '/change',
       name: 'change',
       // route level code-splitting
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import { ref } from "vue";
 import { useAuthStore } from "@/store/AuthStore.js";
 import router from "../router";
@@ -29,14 +29,37 @@ const handleRegister = async () => {
     if(resUser.status == 200) router.push("/dashboard")
 
 };
-</script>
+</script> -->
 
 <template>
 <main>
     
-    <div class="registrierungs-container">
+    
     <form @submit.prevent="handleRegister">
         
+        
+    <div>
+        <div class="header">
+            <div class="logo">
+                <img src="../assets/Nexus0.jpg" alt="Das Logo" class="logo">
+            </div>
+     
+            <div>
+                <p class="without-logging-in">Continue without logging in?</p>
+            </div>
+        </div>
+    </div>
+            <div class="text-container">
+                <H1>Tech & Game Nexus</H1>
+    
+                <div class="text">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    </p>
+                </div>
+            </div>
+
+    <div class="container-regist">
         <div class="form-group">
             <label for="name">Name</label>
             <input class="placholder" type="text" id="name" name="name" v-model="name" required>
@@ -56,9 +79,76 @@ const handleRegister = async () => {
             <label for="password_confirmation">Password</label>
             <input class="placholder" type="password" id="password_confirmation" name="password_confirmation" v-model="password_confirmation" required>
         </div>
-  
+    </div>
+        
         <button type="submit">Register</button>
     </form>
+
+    <div class="footer">
+        <p>© 2024 Tech & Game Nexus       Terms</p>
     </div>
+    
+
 </main>
 </template>
+
+<style>
+body {
+  background-image: url('../assets/bg.svg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+@media (min-width: 768px) {
+.header{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-right: 117px;
+  padding-top: 5%;
+}
+.without-logging-in{
+  color: #FFFF;
+  font-size: 16px;
+}
+
+.logo{
+  width: 106px;
+  height: 64px;
+  border-radius: 40px;
+  margin-left: 5%;
+}
+h1{
+  color: #D7A8FC;
+  padding-top: 22px;
+  font-size: 32px;
+}
+.text{
+  padding-top: 22px;
+  color: #FFFF;
+  width: 352px;
+  font-size: 24px;
+}
+.container-regist{
+    display: flex;
+    flex-direction: column;
+    color: #FFFF;
+    width: 379px;
+    height: 269px;
+}
+.form-group{
+    display: flex;
+    flex-direction: column;
+}
+.footer{
+        color: #FFFF;
+        display: flex;
+        justify-content: center;
+        bottom: 15px;
+        position: fixed;
+        width: 100%;
+        font-size: 16px;
+}
+
+}
+</style>
