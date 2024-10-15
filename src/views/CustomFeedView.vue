fe4d8f2cd1eed4066bc53df5233191ca2c95fffc
Dani <dhuber88@sunrise.ch>
2024-10-15 13:37:32 +0200

    Customfeed View fix

--- src/views/CustomFeedView.vue
+++ src/views/CustomFeedView.vue
@@ -24,6 +24,8 @@ const selectedCategories = ref([]);
 onMounted(async () => {
   try {
     const response = await CategoryService.getCategories();
+    console.log("API-Antwort:", response); // Logge die gesamte Antwort
+    console.log("Kategorie-Daten:", response.data); // Logge nur die Daten
     categories.value = response.data;
   } catch (error) {
     console.error("Fehler beim Laden der Kategorien:", error);
